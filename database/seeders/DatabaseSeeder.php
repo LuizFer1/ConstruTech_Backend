e73403f2c8c46759361b32fcb56737f0aa13c582
Daniel Santos <daniel.santos@aluno.unifapce.edu.br>
2025-04-11 01:42:13 -0300

    feat: :sparkles: Atualização para rodar a seed na tabela colaboradores

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
