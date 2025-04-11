0d9409f79be504d95c20b05f38cc1c7783bcb26e
Daniel Santos <daniel.santos@aluno.unifapce.edu.br>
2025-04-11 01:44:50 -0300

    feat: :sparkles: Adicionado o seeder ColaboradorSeeder para popular a tabela colaboradores

---
+++ database/seeders/ColaboradorSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Colaborador;
+
+class ColaboradorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+public function run()
+{
+    Colaborador::create([
+        'nome_completo' => 'João da Silva',
+        'apelido' => 'Joãozinho',
+        'cpf' => '123.456.789-00',
+        'cargo' => 'Analista',
+        'setor' => 'TI',
+        'vinculo' => 'CLT',
+        'matricula' => '2025001',
+        'data_admissao' => '2024-01-10',
+        'email' => 'joao@empresa.com',
+        'telefone' => '(88) 99999-9999',
+        'cep' => '63000-000',
+        'municipio' => 'Juazeiro do Norte',
+        'endereco' => 'Rua Exemplo, 123',
+        'uf' => 'CE',
+        'complemento' => 'Apto 101',
+    ]);
+}
+
+}
