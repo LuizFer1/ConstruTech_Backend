3d0765b4b854a9072fa75dd73aacf93488da5575
Daniel Santos <daniel.santos@aluno.unifapce.edu.br>
2025-04-11 01:45:05 -0300

    feat: :sparkles: Adicionada a rota para o recurso colaboradores utilizando ColaboradorController

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,18 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ClienteController;
-use App\Http\Controllers\ObraController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\ProgressOfWorkController;
+use App\Http\Controllers\ObraController;
+use App\Http\Controllers\ColaboradorController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\AuthController;
 
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::group(['prefix' => 'auth'], function () {
-    Route::post('logout', [AuthController::class,'logout']);
-    Route::post('refresh', [AuthController::class,'refresh']);
-    Route::get('me', [AuthController::class,'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('me', [AuthController::class, 'me']);
 })->middleware('logged');
 
 
@@ -36,9 +37,10 @@ Route::apiResource('users', 'App\Http\Controllers\UserController')->only([
     'destroy'
 ]);
 
-Route::apiResource('obra', ObraController::class)->except(['edit', 'create'])->middleware('logged')->missing(function (Request $request){
+Route::apiResource('obra', ObraController::class)->except(['edit', 'create'])->middleware('logged')->missing(function (Request $request) {
     return response()->json(['message' => 'Obra não encontrada!'], 404);
 });
-Route::apiResource('cliente', ClienteController::class)->except(['edit', 'create'])->middleware('logged')->missing(function(Request $request){
+Route::apiResource('cliente', ClienteController::class)->except(['edit', 'create'])->middleware('logged')->missing(function (Request $request) {
     return response()->json(['message' => 'Cliente não encontrado'], 404);
 });
+Route::apiResource('colaboradores', ColaboradorController::class);
