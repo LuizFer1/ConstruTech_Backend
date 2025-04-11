b03ce5a20563a70f5cf85a356c691c494cea605c
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2025-04-11 11:54:55 -0300

    feat: :sparkles: Alterar autenticação para usar JWT. Rotas de autenticação criadas (prefixo "auth")

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,26 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::get('me', [AuthController::class,'me']);
+})->middleware('logged');
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('users', 'App\Http\Controllers\UserController')->only([
-    'index', 'store', 'show', 'update', 'destroy'
+    'index',
+    'store',
+    'show',
+    'update',
+    'destroy'
 ]);
